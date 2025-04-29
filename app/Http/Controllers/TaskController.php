ed494d9711dd252d95d12cc68f292eb570f5fe4d
Ronald-pro <36841157+Ronald-pro@users.noreply.github.com>
2025-04-29 13:31:03 +0300

    error handling, code debugging and added pagination

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Task;
+use Exception;
 
 class TaskController extends Controller
 {
@@ -12,21 +13,34 @@ class TaskController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        $tasks = Task::orderBy('created_at', 'DESC')->get();
-        if ($tasks)
+        try
         {
+            $perPage = $request->get('per_page', 10);
+
+            $tasks = Task::orderBy('created_at', 'DESC')->paginate($perPage);
+
             return response([
-                'success' => 'true',
-                'message' => 'Tasks successfull retrieved',
-                'data' => $tasks
+                'success' => true,
+                'message' => $tasks->isEmpty()
+                  ? 'No list of tasks found '
+                  : 'Tasks successfull retrieved',
+                'data' => $tasks,
+                'meta' => [
+                    'current_page' => $tasks->currentPage(),
+                    'last_page' => $tasks->lastPage(),
+                    'per_page' => $tasks->perPage(),
+                    'total' => $tasks->total()
+                ]
             ], 200);
-        }else{
+        } catch (\Exception $e) {
+
             return response([
-                'success' => 'false'
-                'message' => 'No list of tasks found',
-            ], 404);
+                'success' => false,
+                'message' => 'failed to retrieve task',
+                'error' => $e->getMessage()
+            ], 500);
         }
     }
 
@@ -48,30 +62,40 @@ class TaskController extends Controller
      */
     public function store(Request $request)
     {
-        $request->validate([
-            'title'=>'required'
-        ]);
-        $task = Task::create([
-            'title' => $request->title,
-            'description' => $request->description,
-            'priority' => $request->priority,
-            'due_date' => $request->due_date,
-
-        ]);
-
-        if ($task)
+        try
         {
-            return response->json([
-                'success' => 'true',
-                'message' => 'successfully created the task',
-                'data' => $task
-            ], 201);
-        } else {
+            $request->validate([
+                'title'=>'required'
+            ]);
+            $task = Task::create([
+                'title' => $request->title,
+                'description' => $request->description,
+                'priority' => $request->priority,
+                'due_date' => $request->due_date
+            ]);
+
+            if ($task)
+            {
+                return response([
+                    'success' => true,
+                    'message' => 'successfully created the task',
+                    'data' => $task
+                ], 201);
+            } else {
+                return response([
+                    'success' => false,
+                    'message' => 'could not create the task'
+                ], 200);
+            }
+        } catch(\Exception $e) {
+
             return response([
-                'success' => 'false'
-                'message' => 'could not create the task'
+                'success' => false,
+                'message' => 'could not create the task, please try again',
+                'error' => $e->getMessage()
             ], 500);
         }
+
 
     }
 
@@ -83,20 +107,31 @@ class TaskController extends Controller
      */
     public function show($id)
     {
-        $task = Task::find($id);
-        if ($task)
+        try
         {
-            return response->json([
-                'success' => 'true',
-                'message' => 'Task Found Successfully',
-                'data' => $task
-            ], 200);
-        } else {
+            $task = Task::find($id);
+            if ($task)
+            {
+                return response()->json([
+                    'success' => true,
+                    'message' => 'Task Found Successfully',
+                    'data' => $task
+                ], 200);
+            } else {
+                return response()->json([
+                    'success' => false,
+                    'message' => 'Task not found',
+                ], 404);
+            }
+        } catch(\Exception $e) {
+
             return response([
-                'success' => 'false'
-                'message' => 'Task not found',
-            ], 404);
+                'success' => false,
+                'message' => 'failed to find task, please try again',
+                'error' => $e->getMessage()
+            ], 500);
         }
+
     }
 
     /**
@@ -119,30 +154,40 @@ class TaskController extends Controller
      */
     public function update(Request $request, $id)
     {
-        $request->validate([
-            'title'=>'required'
-        ]);
-
-        $task = Task::find($id);
-        if ($task)
+        try
         {
-            $task->title = $request->title;
-            $task->description = $request->description;
-            $task->priority = $request->priority;
-            $task->due_date = $request->due_date;
-            $task->update();
-            return response()->json([
-                'message' => 'Task Updated Successfully',
-                'code' => '200',
-                'status' => 'true',
-                'data' => $task
+            $request->validate([
+                'title'=>'required'
             ]);
-        } else {
-            return response()->json([
-                'success' => 'false'
-                'message' => 'Could not update the task',
+
+            $task = Task::find($id);
+            if ($task)
+            {
+                $task->title = $request->title;
+                $task->description = $request->description;
+                $task->priority = $request->priority;
+                $task->due_date = $request->due_date;
+                $task->update();
+                return response()->json([
+                    'message' => 'Task Updated Successfully',
+                    'status' => true,
+                    'data' => $task
+                ], 200);
+            } else {
+                return response()->json([
+                    'success' => false,
+                    'message' => 'could not update the task',
+                ], 200);
+            }
+        } catch(\Exception $e) {
+
+            return response([
+                'success' => false,
+                'message' => 'could not update the task, please try again',
+                'error' => $e->getMessage()
             ], 500);
         }
+
     }
 
     /**
@@ -153,19 +198,29 @@ class TaskController extends Controller
      */
     public function destroy($id)
     {
-        $task = Task::find($id);
-        if ($task)
-        {
-            $task->delete();
-            return response()->json([
-                'success' => 'true'
-                'message' => 'Task Deleted Successfully',
-            ], 200);
-        } else {
-            return response()->json([
-                'success' => 'false'
-                'message' => 'Could not delete task',
+        try{
+            $task = Task::find($id);
+            if ($task)
+            {
+                $task->delete();
+                return response([
+                    'success' => true,
+                    'message' => 'Task Deleted Successfully',
+                ], 200);
+            } else {
+                return response([
+                    'success' => false,
+                    'message' => 'could not delete task, task not found',
+                ], 404);
+            }
+        } catch(\Exception $e) {
+
+            return response([
+                'success' => false,
+                'message' => 'could not delete task, please try again',
+                'error' => $e->getMessage()
             ], 500);
         }
+
     }
 }
